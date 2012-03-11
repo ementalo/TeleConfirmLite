b3081e3c6a2e3c90be4bef57c9eac2e72570b92c
ementalo <ementalodev@gmx.co.uk>
2012-03-11 17:37:36 +0000

    v2.0.6 -Update to use bperms latest api. 1.2.3 support

--- src/com/ementalo/tcl/Permissions/bPermissionsPerms.java
+++ src/com/ementalo/tcl/Permissions/bPermissionsPerms.java
@@ -1,17 +1,13 @@
 package com.ementalo.tcl.Permissions;
 
-import de.bananaco.permissions.Permissions;
-import de.bananaco.permissions.interfaces.PermissionSet;
-import de.bananaco.permissions.worlds.HasPermission;
+import de.bananaco.bpermissions.api.ApiLayer;
+import de.bananaco.bpermissions.api.util.CalculableType;
 import org.bukkit.entity.Player;
 
 public class bPermissionsPerms extends PermissionsBase{
 
     @Override
     public boolean hasPermission(Player player, String node) {
-        PermissionSet set = Permissions.getWorldPermissionsManager().getPermissionSet(player.getWorld());
-        if (set == null)
-            return false;
-        return HasPermission.has(player, node);
-    }
+        return ApiLayer.hasPermission(player.getWorld().getName(), CalculableType.USER, player.getName(), node);
+     }
 }
