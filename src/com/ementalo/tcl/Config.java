eec3723d218dffbc9ad338381b529119ddd1f911
Zora <MrZoraman@Gmail.com>
2014-07-08 21:55:24 -0700

    File encodings were causing issues

--- src/com/ementalo/tcl/Config.java
+++ src/com/ementalo/tcl/Config.java
@@ -1,5 +1,6 @@
 package com.ementalo.tcl;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.Configuration;
 
 public class Config {
@@ -30,29 +31,29 @@ public class Config {
     }
 
     public void AssignSettings() {
-        accepteeMsg = config.getString("messages.accepteeMsg", "Accepted request.");
-        denyeeMsg = config.getString("messages.denyeeMsg", "Denied request.");
-        accepterMsg = config.getString("messages.accepterMsg", "Request to teleport accepted.");
-        denierMsg = config.getString("messages.denierMsg", "Request to teleport denied.");
-        fromMsg = config.getString("messages.fromMsg", "%p would like to %t");
-        tpToThemMsg = config.getString("messages.tpToThemMsg", "teleport to you");
-        tpThemToYouMsg = config.getString("messages.tpThemToYouMsg", "teleport you to them");
-        acceptDenyPrompt = config.getString("messages.acceptDenyPrompt", "To accept this, type /%a. To deny, type /%d");
-        playerHasPendingReq = config.getString("messages.playerHasPendingReq", "That player already has a pending request!");
-        requestSent = config.getString("messages.requestSent", "Request sent");
-        playerNotFound = config.getString("messages.playerNotFound", "§cPlayer %p not found");
-        playerCommand = config.getString("messages.playerCommand", "§cThis command is only available in game to players");
-        selfTp = config.getString("messages.selfTp", "§cYou cannot tp to yourself");
-        toggledMsg = config.getString("messages.toggledMsg", "§c%p §7 is not accepting requests");
-        backUsage = config.getString("messages.backUsage", "§7Teleporting you to your previous location");
-        noBackLocation = config.getString("messages.noBackLocation", "§7No previous location");
-        removeReq = config.getString("messages.removeReq", "§7Requests removed");
-        teleportation = config.getString("messages.teleportation", "Teleportation");
-        allowed = config.getString("messages.allowed", "Allowed");
-        disabled = config.getString("messages.disabled", "Disabled");
-        permissionDenied = config.getString("messages.permission", "§7You do not have permission for that command");
-        requestWorlds = config.getString("messages.requestWorlds", "§7You cannot request to teleport from different worlds");
-        noPendingRequests = config.getString("messages.noCurrentRequest", "§7You do not have any pending requests");
+        accepteeMsg = ChatColor.translateAlternateColorCodes        ('&', config.getString("messages.accepteeMsg", "Accepted request."));
+        denyeeMsg = ChatColor.translateAlternateColorCodes          ('&', config.getString("messages.denyeeMsg", "Denied request."));
+        accepterMsg = ChatColor.translateAlternateColorCodes        ('&', config.getString("messages.accepterMsg", "Request to teleport accepted."));
+        denierMsg = ChatColor.translateAlternateColorCodes          ('&', config.getString("messages.denierMsg", "Request to teleport denied."));
+        fromMsg = ChatColor.translateAlternateColorCodes            ('&', config.getString("messages.fromMsg", "%p would like to %t"));
+        tpToThemMsg = ChatColor.translateAlternateColorCodes        ('&', config.getString("messages.tpToThemMsg", "teleport to you"));
+        tpThemToYouMsg = ChatColor.translateAlternateColorCodes     ('&', config.getString("messages.tpThemToYouMsg", "teleport you to them"));
+        acceptDenyPrompt = ChatColor.translateAlternateColorCodes   ('&', config.getString("messages.acceptDenyPrompt", "To accept this, type /%a. To deny, type /%d"));
+        playerHasPendingReq = ChatColor.translateAlternateColorCodes('&', config.getString("messages.playerHasPendingReq", "That player already has a pending request!"));
+        requestSent = ChatColor.translateAlternateColorCodes        ('&', config.getString("messages.requestSent", "Request sent"));
+        playerNotFound = ChatColor.translateAlternateColorCodes     ('&', config.getString("messages.playerNotFound", "&cPlayer %p not found"));
+        playerCommand = ChatColor.translateAlternateColorCodes      ('&', config.getString("messages.playerCommand", "&cThis command is only available in game to players"));
+        selfTp = ChatColor.translateAlternateColorCodes             ('&', config.getString("messages.selfTp", "&cYou cannot tp to yourself"));
+        toggledMsg = ChatColor.translateAlternateColorCodes         ('&', config.getString("messages.toggledMsg", "&c%p &7 is not accepting requests"));
+        backUsage = ChatColor.translateAlternateColorCodes          ('&', config.getString("messages.backUsage", "&7Teleporting you to your previous location"));
+        noBackLocation = ChatColor.translateAlternateColorCodes     ('&', config.getString("messages.noBackLocation", "&7No previous location"));
+        removeReq = ChatColor.translateAlternateColorCodes          ('&', config.getString("messages.removeReq", "&7Requests removed"));
+        teleportation = ChatColor.translateAlternateColorCodes      ('&', config.getString("messages.teleportation", "Teleportation"));
+        allowed = ChatColor.translateAlternateColorCodes            ('&', config.getString("messages.allowed", "Allowed"));
+        disabled = ChatColor.translateAlternateColorCodes           ('&', config.getString("messages.disabled", "Disabled"));
+        permissionDenied = ChatColor.translateAlternateColorCodes   ('&', config.getString("messages.permission", "&7You do not have permission for that command"));
+        requestWorlds = ChatColor.translateAlternateColorCodes      ('&', config.getString("messages.requestWorlds", "&7You cannot request to teleport from different worlds"));
+        noPendingRequests = ChatColor.translateAlternateColorCodes  ('&', config.getString("messages.noCurrentRequest", "&7You do not have any pending requests"));
 
         //settings
         clearRequestsOnWorldChange = config.getBoolean("settings.clearRequestsOnWorldChange", false);
